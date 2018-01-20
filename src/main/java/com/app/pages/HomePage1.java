bde21c46fc7ac2f0aa94fbb208a41a2eb544d314
gyuvaraj10 <gyuvaraj10@gmail.com>
2018-01-20 16:57:31 +0000

    Creating the page class

---
+++ src/main/java/com/app/pages/HomePage1.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage1 {
+
+   @FindBy(xpath = "//username")
+   private WebElement username1;
+
+
+
+}
